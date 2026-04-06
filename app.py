1fc2c06235ccb3fd05b18a5f3f763fdd7532fba4
migmory <mmoreno@nexwellpower.com>
2026-04-06 18:09:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
+st.set_page_config(page_title="Spanish Energy Market App", layout="wide")
+
 st.title("Spanish Energy Market App")
-st.write("Hello world")
+st.write("Select a section from the sidebar:")
+st.markdown("- Day Ahead")
+st.markdown("- Forward Market")
+st.markdown("- BESS")
