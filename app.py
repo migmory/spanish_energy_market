3e90f2a19a54eb480db5f9c025e36f988df468b1
migmory <mmoreno@nexwellpower.com>
2026-05-12 16:17:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent
 LOGO_PATH = BASE_DIR / "data" / "nexwell-power-.jpg"
 
 if LOGO_PATH.exists():
-    st.logo(str(LOGO_PATH))
-
+    st.sidebar.image(str(LOGO_PATH), width=140)   # prueba 140, 160 o 180
 
 st.set_page_config(
     page_title="Spanish Energy Market App",
@@ -15,13 +14,13 @@ st.set_page_config(
 )
 
 pg = st.navigation([
-    st.Page("pages/0_Home.py", title="Home"),
-    st.Page("pages/1_Day_Ahead.py", title="Day Ahead"),
-    st.Page("pages/2_Forward_Market.py", title="Forward Market"),
-    st.Page("pages/3_BESS.py", title="BESS"),
-    st.Page("pages/4_Email_Report.py", title="Email Report"),
-    st.Page("pages/5_MIBGAS.py", title="MIBGAS"),
-    st.Page("pages/test.py", title="test"),
+    st.Page("pages/0_Home.py", title="Home", icon="🏠"),
+    st.Page("pages/1_Day_Ahead.py", title="Day Ahead", icon="⚡"),
+    st.Page("pages/2_Forward_Market.py", title="Forward Market", icon="📈"),
+    st.Page("pages/3_BESS.py", title="BESS", icon="🔋"),
+    st.Page("pages/4_Email_Report.py", title="Email Report", icon="📧"),
+    st.Page("pages/5_MIBGAS.py", title="MIBGAS", icon="🔥"),
+    st.Page("pages/test.py", title="test", icon="🧪"),
 ])
 
 pg.run()
